63e301f42cbf1975e5ebe739f22e0dc3c5acd34c
ybkim-dev <beanybigggy@gmail.com>
2022-05-08 23:15:41 +0900

    Feat: "추가 Post Entity"
    추후 다양한 Post 상속 클래스 생성을 위한 Post Entity 추가
    
    Resolved: [#11]

---
+++ src/main/java/com/devsian/post/entity/Post.java
@@ -0,0 +1,23 @@
+package com.devsian.post.entity;
+
+import com.devsian.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@ToString
+public class Post {
+    Integer id;
+    String title;
+    String content;
+    Integer boardId;
+    ArrayList<String> images;
+    User writer;
+    Integer views;
+    ZonedDateTime createdAt;
+}
